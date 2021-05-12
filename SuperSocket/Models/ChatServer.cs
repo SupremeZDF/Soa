169e7ef257c275909691a56fed75458bd967a8e1
东风 赵 <zdf@cheok.com>
2021-05-12 17:46:58 +0800

    代码提交

--- SuperSocket/Models/ChatServer.cs
+++ SuperSocket/Models/ChatServer.cs
@@ -1,14 +1,18 @@
 ﻿using SuperSocket.SocketBase;
 using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Protocol;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace SuperSocket.Models
 {
     public class ChatServer : AppServer<OneModel>
     {
+        public ChatServer(): base(new CommandLineReceiveFilterFactory(encoding:Encoding.UTF8)) { }
+
         protected override bool Setup(IRootConfig rootConfig, IServerConfig config)
         {
             return base.Setup(rootConfig, config);
@@ -22,6 +26,11 @@ namespace SuperSocket.Models
         protected override void OnStopped()
         {
             base.OnStopped();
-        } 
+        }
+
+        protected override void OnNewSessionConnected(OneModel session)
+        {
+            base.OnNewSessionConnected(session);
+        }
     }
 }
